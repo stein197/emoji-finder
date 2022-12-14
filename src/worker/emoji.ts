a50ea39915320efed14580f02c3c70c75dc07876
Nail Gafarov <nil20122013@gmail.com>
2022-12-14 21:22:46 +0500

    Completed emoji worker

--- src/worker/emoji.ts
+++ src/worker/emoji.ts
@@ -1,15 +1,27 @@
+import * as u from "u";
+import type {Emoji} from "type/Emoji";
 import type {EmojiWorkerRequest} from "type/EmojiWorkerRequest";
 import type {EmojiWorkerResponse} from "type/EmojiWorkerResponse";
-import * as u from "u";
 
 const URL_DATA = "/emoji.json";
+const REGEX_SPACE = /\s+/g;
 
-// TODO
 window.onmessage = async (e: MessageEvent<EmojiWorkerRequest>): Promise<void> => {
-	e;
-	u.loadJSON(URL_DATA);
-	const result: EmojiWorkerResponse = {
-		data: []
-	};
-	window.postMessage(result);
+	try {
+		const data = await u.loadJSON<Emoji[]>(URL_DATA);
+		const qTags = e.data.q.split(REGEX_SPACE).filter(tag => tag);
+		const result: EmojiWorkerResponse = {
+			id: e.data.id,
+			data: data.filter(emoji => qTags.every(qTag => emoji.tags.some(emojiTag => emojiTag.indexOf(qTag) >= 0)))
+		};
+		window.postMessage(result);
+	} catch (err) {
+		const result: EmojiWorkerResponse = {
+			id: e.data.id,
+			error: {
+				message: String(err)
+			}
+		};
+		window.postMessage(result);
+	}
 }
